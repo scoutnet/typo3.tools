55e10ef8d0b6cb94cd5142fe78bab4f9b5f9a31b
DasLampe <daslampe@lano-crew.org>
2018-01-05 13:15:14 +0100

    Modify: use lowercase

--- createPluginStructure.sh
+++ createPluginStructure.sh
@@ -4,7 +4,7 @@ mkdir -p Classes/{Controller,Helpers,ViewHelpers,Domain/{Model,Repository}}
 
 mkdir -p Configuration/TCA
 
-mkdir -p Resources/{Private/{Language,Layouts,Partials,Templates},Public/{CSS,JS,Icons}}
+mkdir -p Resources/{Private/{Language,Layouts,Partials,Templates},Public/{css,js,Icons}}
 
 cat $(dirname $0)/template_files/ext_emconf.template > ext_emconf.php
 
